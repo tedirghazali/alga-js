82ca374c85859f96e8520722e2ddfa1f73bc203b
Tedir Ghazali <gteuku@gmail.com>
2018-09-08 16:21:29 +0700

    Navigation 0.0.2

--- pidie-0.0.1.js
+++ pidie-0.0.1.js
@@ -17,9 +17,21 @@ class Pidie {
   jendela() {
     document.getElementById("panel").addEventListener("click", function(){
       document.querySelector(".pd-panel").style.display = "block";
+      document.querySelector(".pd-panel").appendChild(document.querySelector("form"));
+      document.querySelector(".pd-panel").appendChild(document.querySelector(".pd-menu-left"));
+      document.querySelector(".pd-panel").appendChild(document.querySelector(".pd-menu-right"));
+      document.querySelector(".pd-nav").style.height = "60px";
+      document.querySelector(".pd-menu-left").style.display = "block";
+      document.querySelector(".pd-menu-right").style.display = "block";
     });
     document.querySelector(".pd-panel").addEventListener("click", function(){
       document.querySelector(".pd-panel").style.display = "none";
+      document.querySelector(".pd-nav").appendChild(document.querySelector(".pd-menu-left"));
+      document.querySelector(".pd-nav").appendChild(document.querySelector("form"));
+      document.querySelector(".pd-nav").appendChild(document.querySelector(".pd-menu-right"));
+      document.querySelector(".pd-nav").style.height = "auto";
+      document.querySelector(".pd-menu-left").style.display = "";
+      document.querySelector(".pd-menu-right").style.display = "";
     });
   }
 }
