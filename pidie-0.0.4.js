9eb59d1a2205e4a14bd3deceff0790154b178b65
Tedir Ghazali <tedirghazali@gmail.com>
2018-10-09 18:17:09 +0700

    Images Compare

--- pidie-0.0.4.js
+++ pidie-0.0.4.js
@@ -10,6 +10,59 @@ class Pidie {
   constructor(){}
 
   // methods v0.0.3
+  imageCompare() {
+    var x, i;
+    x = document.getElementsByClassName("pd-image-compare-overlay");
+    for (i = 0; i < x.length; i++) {
+      compareImages(x[i]);
+    }
+    function compareImages(img) {
+      var slider, img, clicked = 0, w, h;
+      w = img.offsetWidth;
+      h = img.offsetHeight;
+      document.querySelector('.pd-image-comparison').style.height = h + 'px';
+      img.style.width = (w / 2) + "px";
+      slider = document.createElement("div");
+      slider.innerHTML = '<span class="pd-image-compare-icon">&#10094; &#10095;<span>';
+      slider.setAttribute("class", "pd-image-compare-slider");
+      img.parentElement.insertBefore(slider, img);
+      slider.style.top = (h / 2) - (slider.offsetHeight / 2) + "px";
+      slider.style.left = (w / 2) - (slider.offsetWidth / 2) + "px";
+      slider.addEventListener("mousedown", slideReady);
+      window.addEventListener("mouseup", slideFinish);
+      slider.addEventListener("touchstart", slideReady);
+      window.addEventListener("touchstop", slideFinish);
+      function slideReady(e) {
+        e.preventDefault();
+        clicked = 1;
+        window.addEventListener("mousemove", slideMove);
+        window.addEventListener("touchmove", slideMove);
+      }
+      function slideFinish() {
+        clicked = 0;
+      }
+      function slideMove(e) {
+        var pos;
+        if (clicked == 0) return false;
+        pos = getCursorPos(e)
+        if (pos < 0) pos = 0;
+        if (pos > w) pos = w;
+        slide(pos);
+      }
+      function getCursorPos(e) {
+        var a, x = 0;
+        e = e || window.event;
+        a = img.getBoundingClientRect();
+        x = e.pageX - a.left;
+        x = x - window.pageXOffset;
+        return x;
+      }
+      function slide(x) {
+        img.style.width = x + "px";
+        slider.style.left = img.offsetWidth - (slider.offsetWidth / 2) + "px";
+      }
+    }
+  }
   quotesSlideshow() {
     var slideIndex = 1;
     var quotes = document.querySelector('.pd-quotes');
