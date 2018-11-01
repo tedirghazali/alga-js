09090bbfb81f98d09cc7137ea93294b795cc8260
Tedir Ghazali <tedirghazali@gmail.com>
2018-11-01 16:17:43 +0700

    Penambahan Grunt

--- pidie-ui-0.0.1-alpha-1.js
+++
@@ -1,6 +0,0 @@
-/*
-* PidieUi 0.0.1-alpha-1
-* 2019
-* Tedir Ghazali
-* Apache License 2.0
-*/
---
+++ src/pidie-ui-0.0.1-alpha-1.js
@@ -0,0 +1,16 @@
+/*
+* PidieUi 0.0.1-alpha-1
+* 2019
+* Tedir Ghazali
+* Apache License 2.0
+*/
+
+class PidieUi {
+    
+    constructor() {
+    
+    }
+    
+}
+
+new PidieUi()
