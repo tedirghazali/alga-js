f6e718172f7c56bfb0e65df3238a8cedec838c8c
Tedir Ghazali <tedirghazali@gmail.com>
2018-12-04 21:02:34 +0700

    Flag Select Part 5

--- src/pidie-ui-0.0.1-alpha-1.js
+++ src/pidie-ui-0.0.1-alpha-1.js
@@ -10,34 +10,11 @@
     "use strict";
 
     function init() {
-        panel();
+        navbar();
     }
     
-    function panel() {
-        var panel = document.getElementById("panel");
-        var pdPanel = document.querySelector(".pd-panel");
-        var pdNav = document.querySelector(".pd-nav");
-        var pdForm = document.querySelector(".pd-nav-search");
-        var pdMenuLeft = document.querySelector(".pd-menu-left");
-        var pdMenuRight = document.querySelector(".pd-menu-right");
-        panel.addEventListener("click", function(){
-          pdPanel.style.display = "block";
-          //(pdForm)? pdPanel.appendChild(pdForm): "";
-          //(pdMenuLeft)? pdPanel.appendChild(pdMenuLeft): "";
-          //(pdMenuRight)? pdPanel.appendChild(pdMenuRight): "";
-          pdNav.style.height = "60px";
-          pdMenuLeft.style.display = "block";
-          pdMenuRight.style.display = "block";
-        });
-        pdPanel.addEventListener("click", function(){
-          pdPanel.style.display = "none";
-          //(pdMenuLeft)? pdNav.appendChild(pdMenuLeft): "";
-         //(pdForm)? pdNav.appendChild(pdForm): "";
-          //(pdMenuRight)? pdNav.appendChild(pdMenuRight): "";
-          pdNav.style.height = "auto";
-          pdMenuLeft.style.display = "";
-          pdMenuRight.style.display = "";
-        });
+    function navbar() {
+        
     }
     
     init();
