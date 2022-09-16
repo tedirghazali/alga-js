c69ef890afcb0baf6f6f717d768bdf228aec4791
Tedir Ghazali <tedirghazali@gmail.com>
2022-09-16 18:33:13 +0700

    Fix the short version of time relative

--- ts/time/relativeTime.ts
+++ ts/time/relativeTime.ts
@@ -3,7 +3,7 @@ import { daysInBetween } from '../date/numberDate'
 import { doubleFormat } from '../date/stringDate'
 import { hoursInBetween } from './numberTime'
 
-export const relative = (dateValue: Date | string | number, timeZone: string = 'utc', locale: string = 'en-US', options: any = {}, defaultValue: string = 'new') => {
+export const relative = (dateValue: Date | string | number, timeZone: string = 'utc', locale: string = 'en-US', options: any = {}, type: string = 'long') => {
   const nowDate: Date = new Date()
   let valDate: Date = new Date(dateValue)
   if(timeZone !== 'utc') {
@@ -11,32 +11,50 @@ export const relative = (dateValue: Date | string | number, timeZone: string = '
   }
   
   let seconds = Math.round((valDate.valueOf() - nowDate.valueOf()) / 1000)
-  let newTimeRelative = new Intl.RelativeTimeFormat(locale, options).format(Math.round(seconds), 'second')
+  let newTimeRelative = new Intl.RelativeTimeFormat(locale, options).format(, 'second')
+  if(type === 'short') {
+    newTimeRelative = `${Math.round(seconds)}s`
+  }
   
   let interval = seconds / 31536000
   if (Math.abs(interval) > 1) {
     const newInterval = Math.round(interval)
     newTimeRelative = new Intl.RelativeTimeFormat(locale, options).format(newInterval, 'year')
+    if(type === 'short') {
+      newTimeRelative = `${Math.round(newInterval)}y`
+    }
   } else {
     interval = seconds / 2592000
     if (Math.abs(interval) > 1) {
       const newInterval = Math.round(interval)
       newTimeRelative = new Intl.RelativeTimeFormat(locale, options).format(newInterval, 'month')
+      if(type === 'short') {
+        newTimeRelative = `${Math.round(newInterval)}mo`
+      }
     } else {
       interval = seconds / 86400;
       if (Math.abs(interval) > 1) {
         const newInterval = Math.round(interval)
         newTimeRelative = new Intl.RelativeTimeFormat(locale, options).format(newInterval, 'day')
+        if(type === 'short') {
+          newTimeRelative = `${Math.round(newInterval)}d`
+        }
       } else {
         interval = seconds / 3600;
         if (Math.abs(interval) > 1) {
           const newInterval = Math.round(interval)
           newTimeRelative = new Intl.RelativeTimeFormat(locale, options).format(newInterval, 'hour')
+          if(type === 'short') {
+            newTimeRelative = `${Math.round(newInterval)}h`
+          }
         } else {
           interval = seconds / 60;
           if (Math.abs(interval) > 1) {
             const newInterval = Math.round(interval)
             newTimeRelative = new Intl.RelativeTimeFormat(locale, options).format(newInterval, 'minute')
+            if(type === 'short') {
+              newTimeRelative = `${Math.round(newInterval)}m`
+            }
           }
         }
       }
---
+++ types/string/spaceString.d.ts
@@ -0,0 +1,1 @@
+export declare const spaceBetween: (numArg: string | number) => string;
